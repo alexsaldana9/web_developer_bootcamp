105e0b41d9737228a13ebd8f1573dcad8579b072
Saldana <asaldana@mdc.edu>
2019-06-05 14:55:03 -0400

    object

---
+++ objects/problemset.js
@@ -0,0 +1,5 @@
+var movies = [{title:"star wars", hasWatched: true, rating:10},
+{title:"oliver and company", hasWatched: false, rating: 9},
+{title:"101 dalmatians", hasWatched:true, rating: 10}];
+
+console.log(movies);
