cfe050e6edabf10e923713b48af108d24e979f17
Saldana <asaldana@mdc.edu>
2019-09-20 15:27:09 -0400

    new colors button, - still working on it

--- colorgame/colorgame.js
+++ colorgame/colorgame.js
@@ -1,13 +1,12 @@
-var colors = generateRandomColors(6);
-
-//var square1 = document.getElementById("1s");
-
 var squares = document.querySelectorAll(".square");
-var pickedColor = pickColor(colors);
 var colorDisplay = document.getElementById("colorDisplay");
-
 var message = document.getElementById("displayMessage");
+var resetColorsButton = document.getElementById("newColorsBtn");
 
+var colors = generateRandomColors(6);
+var pickedColor = pickColor(colors);
+
+console.log("pickedColor - line 10 =" + pickedColor);
 
 colorDisplay.textContent = pickedColor;
 
@@ -15,17 +14,45 @@ for(var i = 0; i < squares.length ; i++) {
     squares[i].style.backgroundColor = colors[i];
 
     squares[i].addEventListener("click", function(){
+
+        console.log("CLICKED on a SQUARE");
         var clickedColor = this.style.backgroundColor;
 
+        console.log("pickedColor - line 21 == ", pickedColor);
+        console.log("clickedColor - line 22 ==== ", clickedColor);
+
         if(clickedColor === pickedColor){
+            console.log("INSIDE IF@@@@@@");
             displayMessage.textContent = "Correct";
             changeColors(clickedColor);
         }else {
+            console.log("INSIDE IF@@@@@@+++++++");
             this.style.backgroundColor = "black";
             displayMessage.textContent = "Try again!";
         }
     });
 }
+
+
+resetColorsButton.addEventListener("click", function(){
+    var colors = generateRandomColors(6);
+    var pickedColor = pickColor(colors);
+    colorDisplay.textContent = pickedColor;
+
+    console.log("pickedColor inside of reset ===" + pickedColor);
+
+    displayMessage.textContent = "";
+
+    console.log("clicked resetColors button");
+    //console.log("pickedColor === " + pickedColor);
+   
+    for(var i = 0; i < squares.length ; i++) {
+        squares[i].style.backgroundColor = colors[i];
+        console.log("colors[i] > ", colors[i]);
+    }
+});
+
+
 
 function changeColors(color){
     for(var i = 0; i < squares.length; i++){
@@ -38,7 +65,6 @@ function randomColor(){
     var green = Math.floor(Math.random() * 256);
     var blue = Math.floor(Math.random() * 256);
 
-    console.log("rgb(" + red + ", "+ green + ", "+ blue + ")");
 
     return "rgb(" + red + ", "+ green + ", "+ blue + ")";
 }
@@ -50,6 +76,7 @@ function generateRandomColors(num) {
         array.push(randomColor());
     }
 
+    console.log("generateRandomColors!!! runnung");
     return array;
 }
 
