d7adf2930a146d9d2ec67520568dbab97078eda0
Saldana <asaldana@mdc.edu>
2019-09-20 09:37:57 -0400

    color game - styles, show message

--- colorgame/colorgame.js
+++ colorgame/colorgame.js
@@ -6,17 +6,35 @@ var colors = [
     "rgb(255, 0, 255)",
 ];
 
-var square1 = document.getElementById("1s");
+//var square1 = document.getElementById("1s");
 
 var squares = document.querySelectorAll(".square");
+var pickedColor = colors[2];
+var colorDisplay = document.getElementById("colorDisplay");
+
+var message = document.getElementById("displayMessage");
+
+
+colorDisplay.textContent = pickedColor;
 
 for(var i = 0; i < squares.length ; i++) {
     squares[i].style.backgroundColor = colors[i];
+
+    squares[i].addEventListener("click", function(){
+        var clickedColor = this.style.backgroundColor;
+
+        if(clickedColor === pickedColor){
+            displayMessage.textContent = "Correct";
+        }else {
+            this.style.backgroundColor = "black";
+            displayMessage.textContent = "Try again!";
+        }
+    });
 }
 
-$("#1s").click(function(){
-    alert("sdklfjksdjfl");
-    square1.classList.add("done");
-});
+// $("#1s").click(function(){
+//     alert("sdklfjksdjfl");
+//     square1.classList.add("done");
+// });
 
 
