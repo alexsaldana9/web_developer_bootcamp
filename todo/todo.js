6f492356369a6cd143d388768dababb3dc706e16
Saldana <asaldana@mdc.edu>
2019-12-06 10:47:15 -0500

    check if textbox is empty

--- todo/todo.js
+++ todo/todo.js
@@ -1,78 +1,75 @@
 
 
-$("li").on('click', function () {
+// $("li").on('click', function () {
 
-    console.log("clicked li");
-    var gValue = $(this).attr('class');
-    if (gValue == 'orange') {
-        $(this).removeClass("blue");
-        $(this).addClass("red");
-    } else {
-        $(this).removeClass("red");
-        $(this).addClass("blue");
-    }
-});
+//     console.log("clicked li");
+//     var gValue = $(this).attr('class');
+//     if (gValue == 'orange') {
+//         $(this).removeClass("blue");
+//         $(this).addClass("red");
+//     } else {
+//         $(this).removeClass("red");
+//         $(this).addClass("blue");
+//     }
+// });
 
 
 function addItem(){
     var count = 0;
     var textInput  = document.getElementById('todo-input').value;
-    console.log("textInput .." , textInput);
+    textInput.trim();
+    console.log("textInput ....", textInput);
+    console.log("textInput length..", textInput.length);
 
-    var li = document.createElement('li');
-    console.log("li .... ", li);
+    if (textInput === "" || textInput.length === 0 || textInput === null ||  textInput.length === 1) {
+        console.log("enter some todo");
+    } else {
+        var li = document.createElement('li');
     
-    var list = document.createTextNode(textInput);
-    li.appendChild(list);
-
-    console.log("list ....", list);
-
-    let buttonDone = document.createElement('BUTTON'); 
-    buttonDone.textContent = 'Done';
-    buttonDone.className = 'buttonDone';
-    buttonDone.addEventListener("click", itemDone);
-
-    document.getElementById("dynamic-list").appendChild(li);
-    li.appendChild(buttonDone);
-
-    document.getElementById("todo-input").value = " ";
-
-
-    count = $("#dynamic-list li").length
-    console.log("count li >>", count);
-
-    $('#dynamic-list li').each(function(i) {
-        //$(this).attr('id', ("li"+(i+1), 'onmousedown' , 'changeColor(this.id)'));
-
-        $(this).attr({
-            "id": "li"+(i+1),
-            "onmousedown": "itemDone(this.id)"
+        var list = document.createTextNode(textInput);
+        li.appendChild(list);
+    
+        let buttonDone = document.createElement('BUTTON'); 
+        buttonDone.textContent = 'Done';
+        buttonDone.className = 'buttonDone btn btn-secondary';
+        buttonDone.addEventListener("click", itemDone);
+    
+        document.getElementById("dynamic-list").appendChild(li);
+        li.appendChild(buttonDone);
+    
+        document.getElementById("todo-input").value = " ";
+    
+        count = $("#dynamic-list li").length
+    
+        $('#dynamic-list li').each(function(i) {
+            $(this).attr({
+                "id": "li"+(i+1),
+                "onmousedown": "itemDone(this.id)"
+            });
         });
-    });
+    
+        $('#dynamic-list li button').each(function(i) {
+            $(this).attr('id', ('button' + (i+1)));
+        });
 
-    $('#dynamic-list li button').each(function(i) {
-        $(this).attr('id', ('button' + (i+1)));
-    });
+        //$('#todo-input').empty();
+        //textInput.length=0;
+        textInput.trim();
+
+        console.log("textInput lenght after add >>", textInput.length);
+
+    }
+
+    textInput.trim();
 }
 
 function itemDone(id){
-    // var ul = document.getElementById("dynamic-list");
-    // var deleteItem = document.getElementById("dataInput");
-    // var item = document.getElementById(deleteItem.value);
-    // ul.removeChild(item);
-    console.log("selected done button");
-    //$('#dynamic-list ul li.selected').css("color", "red");
-
-
     var listitem = document.getElementById(id);
-    //listitem.classList.add('doneItem');
-
     $(listitem).addClass("doneItem");
-
 }
 
 function clearList(){
-    $('#dynamic-list').empty()
+    $('#dynamic-list').empty();
 }
 
 
