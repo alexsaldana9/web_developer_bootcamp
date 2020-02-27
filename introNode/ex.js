0a268b874cfbee3d4d032e8166ad55495dfe914a
Saldana <asaldana@mdc.edu>
2020-02-27 15:09:17 -0500

    node ex

---
+++ introNode/ex.js
@@ -0,0 +1,12 @@
+// Print out string the number of times of the second argument
+
+echo ("Hello I am number == ", 2);
+
+function echo(string, number){
+    for (var i = 0; i < number ; i++){
+        console.log( string + i );
+    }
+}
+
+
+
