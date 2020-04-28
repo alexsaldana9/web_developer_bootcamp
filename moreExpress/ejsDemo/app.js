9f5c1bc84b1e821986d24ca9bf878bb3c100dca1
Alexandra Saldana <alexsaldana9@gmail.com>
2020-04-28 15:16:56 -0400

    routes

--- moreExpress/ejsDemo/app.js
+++ moreExpress/ejsDemo/app.js
@@ -12,7 +12,6 @@ app.listen(process.env.PORT, process.env.IP, function(){
 app.get("/", function(req, res){
     //res.send("<h1>HOLA</h1>");
     res.render("home.ejs");
-
 });
 
 app.get("/love/:animal", function(req, res){
@@ -21,4 +20,38 @@ app.get("/love/:animal", function(req, res){
 
     //res.send("You love " + animal);
     res.render("love.ejs", {animalVar : animal});
+});
+
+app.get("/repeat/:word/:number", function(req, res){
+		var word = req.params.word.toLowerCase();
+		var num = req.params.number;
+
+		var string = "";
+
+		for (var i = 0; i < num; i++){
+			string =string + " "+  word;
+			//console.log("string  inside loop==", string);
+		}
+		//console.log("output outside loop= ", string);
+    res.send(string);
+});
+
+app.get("/speak/:animal", function(req, res){
+	var animal = req.params.animal.toLowerCase();
+	if(animal === "pig"){
+		var sound = "Oink";
+	} else if(animal === "cow"){
+		var sound = "Mooo";
+	}else if(animal === "dog"){
+		var sound = "Woof woof";
+	} else {
+		var sound = "not found";
+	}
+	res.render("speak.ejs", {animalVar : animal, soundVar : sound});
+});
+
+
+app.get("*", function(req, res){
+    //res.send("<h1>HOLA</h1>");
+    res.render("notfound.ejs");
 });
