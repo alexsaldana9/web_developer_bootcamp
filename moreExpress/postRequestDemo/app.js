0f1a7544f29cefbf1ee6baf3bec81450a05ce2f7
alexsaldana9 <alexsaldana9@gmail.com>
2020-06-15 17:35:09 -0400

    routes

--- moreExpress/postRequestDemo/app.js
+++ moreExpress/postRequestDemo/app.js
@@ -1,5 +1,62 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+var listener = app.listen(8888, function(){
+    console.log('Listening on port ' + listener.address().port); //Listening on port 8888
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server started");
+});
+
+
+
+app.get("/", function(req, res){
+    res.render("home.ejs");
+});
+
+
+app.get("/friend", function(req, res){
+
+    var friends = ["Ale", "John", "Lisa", "Lila"];
+
+    res.render("friends", {friends: friends});
+});
+
+
+app.get("/car", function(req, res){
+
+    var cars = ["toyota", "honda", "jeep", "ford"];
+
+    res.render("cars", {cars: cars});
+});
+
+
+app.get("/post", function(req, res){
+    var posts = ["post1", "post2", "post3"];
+    
+	res.render("posts", {posts: posts});
+});
+
+
+app.post("/addfriend", function(req, res){
+    console.log(req.body);
+    res.send("You reached the post route");
+});
+
+
+app.get("/test", function(req, res){
+
+    res.render("test.ejs");
+});
+
+app.get("*", function(req, res){
+    //res.send("<h1>HOLA</h1>");
+    res.render("notfound");
+});
