7c0da8eacf8b92f23793e4e7d1d30fc802813949
alexsaldana9 <alexsaldana9@gmail.com>
2020-08-11 15:51:49 -0400

    mongoose and mongo for yelp camp app

--- YelpCampv1/app.js
+++ YelpCampv1/app.js
@@ -2,8 +2,70 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1/yelp_camp', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Connected to DB!'))
+.catch(error => console.log(error.message));
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+// SCHEMA
+var campSchema = new mongoose.Schema({
+    name: String,
+    price: Number,
+    location: String
+});
+
+var Camp = mongoose.model("Camp", campSchema);
+
+var campsite =  new Camp({
+    name: "Yosemite", 
+    price: 20, 
+    location: "California"
+});
+
+campsite.save(function(err, response){
+    if(err){
+        console.log("Something went wrong!!");
+    } else {
+        console.log("Saved campsite to DB");
+        console.log(response);
+    }
+});
+
+// New and save 
+Camp.create({
+    name: "Everglades",
+    price: 12,
+    location: "Flamingo"
+}, function(err, response){
+    if(err){
+        console.log("ERROR!!");
+        console.log(err);
+    } else {
+        console.log("New campsite");
+        console.log(response);
+    }
+});
+
+
+// Find all Camps
+Camp.find({}, function(err, response){
+    if(err){
+        console.log("ERROR!!");
+        console.log(err);
+    }else {
+        console.log("All of the camps");
+        console.log(response);
+    }
+})
+// 
+
+
 
 var campgrounds = [ 
     { name: "Redwoods", image: "http://placecorgi.com/260/180" },
