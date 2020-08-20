a4bd2b6e09135033e54261f5967c4c985ab30f02
alexsaldana9 <alexsaldana9@gmail.com>
2020-08-20 17:27:46 -0400

    Post -  show location and images

--- YelpCampv1/app.js
+++ YelpCampv1/app.js
@@ -17,7 +17,9 @@ app.set("view engine", "ejs");
 var campSchema = new mongoose.Schema({
     name: String,
     price: Number,
-    location: String
+    location: String,
+    description: String,
+    image: String
 });
 
 var Camp = mongoose.model("Camp", campSchema);
@@ -25,7 +27,9 @@ var Camp = mongoose.model("Camp", campSchema);
 var campsite =  new Camp({
     name: "Yosemite", 
     price: 20, 
-    location: "California"
+    location: "California",
+    description: "primitive camping",
+    image: "https://www.nps.gov/common/uploads/grid_builder/yose/crop16_9/56CD3D55-E4EF-1B16-0DBD63EB2F5193A4.jpg?width=307&quality=90&mode=crop"
 });
 
 campsite.save(function(err, response){
@@ -38,19 +42,19 @@ campsite.save(function(err, response){
 });
 
 // New and save 
-Camp.create({
-    name: "Everglades",
-    price: 12,
-    location: "Flamingo"
-}, function(err, response){
-    if(err){
-        console.log("ERROR!!");
-        console.log(err);
-    } else {
-        console.log("New campsite");
-        console.log(response);
-    }
-});
+// Camp.create({
+//     name: "Everglades",
+//     price: 12,
+//     location: "Flamingo"
+// }, function(err, response){
+//     if(err){
+//         console.log("ERROR!!");
+//         console.log(err);
+//     } else {
+//         console.log("New campsite");
+//         console.log(response);
+//     }
+// });
 
 
 // Find all Camps
@@ -67,19 +71,41 @@ Camp.find({}, function(err, response){
 
 
 
-var campgrounds = [ 
-    { name: "Redwoods", image: "http://placecorgi.com/260/180" },
-    { name: "Redwoods creek", image: "http://placecorgi.com/260/180" },
-    { name: "Beach", image: "http://placecorgi.com/260/180" }
-];
+// var campgrounds = [ 
+//     { name: "Redwoods", image: "http://placecorgi.com/260/180" },
+//     { name: "Redwoods creek", image: "http://placecorgi.com/260/180" },
+//     { name: "Beach", image: "http://placecorgi.com/260/180" }
+// ];
 
 app.get("/", function(req, res){
     res.render("landing");
 });
 
-app.get("/campgrounds", function(req, res) {
+app.get("/campgrounds/new", function(req, res) {
+    res.render("new.ejs")
+});
 
-    res.render("campgrounds", {campgrounds: campgrounds});
+app.get("/campgrounds/:id", function(req, res) {
+
+
+
+    res.send("THIS will be page for specific id");
+    //res.render("new.ejs")
+});
+
+app.get("/campgrounds", function(req, res) {
+    Camp.find({}, function(err, response){
+        if(err){
+            console.log("ERROR!!");
+            console.log(err);
+        }else {
+            console.log("All of the camps");
+            console.log(response);
+            res.render("index", {campgrounds: response});
+        }
+    })
+
+   
 });
 
 
@@ -89,19 +115,21 @@ app.post("/campgrounds", function(req, res) {
 
     var name = req.body.name;
     var image = req.body.image;
+    var location = req.body.location;
 
-    var newCampground = {name: name, image: image};
+    var newCampground = {name: name, image: image, location: location};
 
-    campgrounds.push(newCampground);
-    res.redirect("/campgrounds");
-
-    
+    Camp.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect("/campgrounds");
+        }
+    });    
 });
 
 
-app.get("/campgrounds/new", function(req, res) {
-    res.render("new.ejs")
-});
+
 // app.listen(process.env.PORT, process.env.IP, function() {
 //     console.log("The YelpCamp Server has started");
 // });
