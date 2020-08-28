5e0bf7e50bb304e70bd3d073fddc89e53da15fa8
alexsaldana9 <alexsaldana9@gmail.com>
2020-08-28 15:00:44 -0400

    views and partials

--- RESTful_Routing/RESTfulBlogApp/app.js
+++ RESTful_Routing/RESTfulBlogApp/app.js
@@ -33,8 +33,18 @@ var Blog = mongoose.model("Blog", blogSchema);
 // });
 
 // Routes
+app.get("/", function(req, res){
+    res.redirect("/blogs");
+})
+
 app.get("/blogs", function(req, res){
-    res.render("index");
+    Blog.find({}, function(err, blogs){
+        if(err){
+            console.log("Error!!");
+        } else {
+            res.render("index", {blogs: blogs});
+        }
+    })
 })
 
 
